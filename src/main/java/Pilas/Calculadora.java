7c275bd1a601998d6c237aa15ec028dd78ef9149
Dilankl30 <lucerodk7@gmial.com>
2024-12-05 11:19:58 -0500

    mi primer commit

---
+++ src/main/java/Pilas/Calculadora.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pilas;
+
+/**
+ *
+ * @author SO-LAB1-PC18
+ */
+public class Calculadora {
+    
+}
---
+++ src/main/java/ec/edu/espoch/julitzaminga2025/JulitzaMinga2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.julitzaminga2025;
+
+/**
+ *
+ * @author SO-LAB1-PC18
+ */
+public class JulitzaMinga2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
